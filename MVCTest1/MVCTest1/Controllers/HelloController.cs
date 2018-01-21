fc431011cd6e1c9d84eb76e81b261b9467721be1
Alba Rosina Sagastume Muralles <alba.sagastume@upsdiv.com>
2018-01-20 23:42:14 -0600

    First MVC project

---
+++ MVCTest1/MVCTest1/Controllers/HelloController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest1.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string WelcomeNoParams()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MVCTest1/MVCTest1/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCTest1.Controllers
+{
+    public class HelloWorldController : ApiController
+    {// 
+        // GET: /HelloWorld/ 
+ 
+        public string Index() 
+        { 
+            return "This is my <b>default</b> action..."; 
+        } 
+ 
+        // 
+        // GET: /HelloWorld/Welcome/ 
+  
+    }
+}
